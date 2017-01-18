8cde49557fb8cc3461b51d130a9dc85bc527a45a
boman <bmnyabuto@gmail.com>
2017-01-19 01:19:16 +0300

    Added a for loop control structure that converts numeric strings in the list created to integers

--- Word_count.py
+++ Word_count.py
@@ -1,2 +1,52 @@
 def words(Text_String):
 	Text_StringList = Text_String.split()
+
+	for i in Text_StringList:
+		if i == '0':
+			j = Text_StringList.index(i)
+            Text_StringList.remove('0')
+            Text_StringList.insert(j, 0)
+                  
+        elif i == '1':
+            j = Text_StringList.index(i)
+            Text_StringList.remove('1')
+            Text_StringList.insert(j, 1)
+
+        elif i == '2':
+            j = Text_StringList.index(i)
+            Text_StringList.remove('2')
+            Text_StringList.insert(j, 2)
+
+        elif i == '3':
+            j = Text_StringList.index(i)
+            Text_StringList.remove('3')
+            Text_StringList.insert(j, 3)
+
+        elif i == '4':
+            j = Text_StringList.index(i)
+            Text_StringList.remove('4')
+            Text_StringList.insert(j, 4)
+
+        elif i == '5':
+            j = Text_StringList.index(i)
+            Text_StringList.remove('5')
+            Text_StringList.insert(j, 5)
+        elif i == '6':
+            j = Text_StringList.index(i)
+            Text_StringList.remove('6')
+            Text_StringList.insert(j, 6)
+
+        elif i == '7':
+            j = Text_StringList.index(i)
+            Text_StringList.remove('7')
+            Text_StringList.insert(j, 7)
+
+        elif i == '8':
+            j = Text_StringList.index(i)
+            Text_StringList.remove('8')
+            Text_StringList.insert(j, 8)
+
+        elif i == '9':
+            j = Text_StringList.index(i)
+            Text_StringList.remove('9')
+            Text_StringList.insert(j, 9)
